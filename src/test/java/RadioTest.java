c50d240612496e43de7c27fb053c1583e14ff2a2
птичка <Sapunova270301@gmail.com>
2023-04-09 18:16:46 +0300

    change pom.xml

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
 
-    @org.testng.annotations.Test
+    @Test
     public void valueInLimit() {        //1
         Radio radio = new Radio();
 
